93b2d6e31d1070c13ebfd788d6373d22500d77c7
sha <urabummer4@hotmail.com>
2017-09-12 17:39:57 +0800

    added basic mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: 'pages#index'
+    resources :users
+    resources :albums
+    root to: 'pages#index'
 end
