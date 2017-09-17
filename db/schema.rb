cc47552cabcd54312bd4b17ccf2ff994e67798fc
tetradodeca <31119683+tetradodeca@users.noreply.github.com>
2017-09-17 15:33:22 +0800

    Revert "Weekend branch"

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170916104905) do
-
+ActiveRecord::Schema.define(version: 20170916100627) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -43,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170916104905) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -55,7 +54,6 @@ ActiveRecord::Schema.define(version: 20170916104905) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
